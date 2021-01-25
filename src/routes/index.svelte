b9fbb660134ca04abad1b861ed2c85cb4013c7ab
Juan Carlos del Rio <jcdre@hotmail.com>
2021-01-25 17:54:52 -0500

    Config, sapper routes structures, Layout, Newslett

---
+++ src/routes/index.svelte
@@ -0,0 +1,34 @@
+<script context="module">
+  export function preload() {
+    return this.fetch(`blog.json`)
+      .then((r) => r.json())
+      .then((posts) => {
+        return { posts };
+      });
+  }
+</script>
+
+<script>
+  import Post from "../components/Post.svelte";
+  export let posts;
+</script>
+
+<svelte:head>
+  <title>Juan Carlos Del Rio's blog</title>
+</svelte:head>
+
+<div class="Home">
+  <div class="Posts">
+    {#each posts as post}
+      <Post {post} />
+    {/each}
+  </div>
+</div>
+
+<style>
+  .Posts {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 15px;
+  }
+</style>
