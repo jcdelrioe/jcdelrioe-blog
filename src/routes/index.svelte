afa9bd6867d5143989b3830bddc6e85e3d1bf6e0
Juan Carlos del Rio <jcdre@hotmail.com>
2021-01-26 17:47:45 -0500

    SEO changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,6 +15,29 @@
 
 <svelte:head>
   <title>Juan Carlos Del Rio's blog</title>
+  <meta name="description" content="Blog de delrioe live" />
+
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@jcdelrioe" />
+  <meta name="twitter:creator" content="@jcdelrioe" />
+  <meta name="twitter:title" content="Juan Carlos Del Rio" />
+  <meta name="twitter:description" content="Blog de delrioe live" />
+  <meta
+    name="twitter:image"
+    content="https://arepa.s3.amazonaws.com/blogpost-cover.png"
+  />
+
+  <meta name="og:title" content="Juan Carlos Del Rio" />
+  <meta name="og:description" content="Blog de delrioe live" />
+  <meta
+    name="og:image"
+    content="https://arepa.s3.amazonaws.com/blogpost-cover.png"
+  />
+  <meta name="og:url" content="https://blog.delrioe.live" />
+  <meta name="og:site_name" content="Juan Carlos Del Rio" />
+  <meta name="og:locale" content="es_ES" />
+  <meta name="og:type" content="article" />
+  <meta name="fb:app_id" content="" />
 </svelte:head>
 
 <div class="Home">
